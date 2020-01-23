f494f8707618ab817c666e01a01ee361b86086a9
Gaven Finch <gavenfinch@gmail.com>
2020-01-23 14:02:25 -0700

    Finish SHA1 wrapper

--- hash_attack.py
+++ hash_attack.py
@@ -1,9 +1,15 @@
 import hashlib
+import sys
 
-def SHA1_wrapper(str, n): #input string, number of bits
-    pass
+def SHA1_wrapper(input, n): #input string, number of bits
+    mask = (2**n)-1 #produces a number whose binary representation holds a '1' in the lower n bits
 
-testString = "This is a test"
-digest = hashlib.sha1(testString.encode('utf-8')).hexdigest()
-digest = bytearray(digest)
-print(digest[-8:])
+    hashObject = hashlib.sha1(input.encode('utf-8'))
+    digest = hashObject.digest()
+    digest_int = int.from_bytes(digest, sys.byteorder)
+    masked_digest = digest_int & mask
+    print(hex(masked_digest))   
+
+
+
+SHA1_wrapper("this is a test", 16)
